18cbc0cc74effecaf16f49c32f85ca011e44c55b
Siyana <roronoazorri@gmail.com>
2022-11-03 16:11:55 +0200

    Adding data class GameData

--- Unity/BalkanGame/src/GameData.cs
+++ Unity/BalkanGame/src/GameData.cs
@@ -1,3 +1,15 @@
+using System.Collections.Generic;
 public class GameData{
+    GameInterface display=new GameInterface();
+    string playerName;
+
+    List<Team> teams = new List<Team>();
+
+    Team currentTeam;
+    Hero currentHero;
+
+    state currentState;
+    Team winner;
+
     
 }
