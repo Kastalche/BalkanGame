18acc4475e707165937f6b8d44d2a1ded3c0df54
Siyana <roronoazorri@gmail.com>
2022-11-08 14:46:28 +0200

    Tragic atack event, needs testing

--- Unity/BalkanGame/src/States/BattleState.cs
+++ Unity/BalkanGame/src/States/BattleState.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using System.Net.Sockets;
 using System.Net.Http;
 using System.Security.Cryptography;
@@ -10,9 +11,14 @@ using BalkanGame.States;
 
 namespace BalkanGame.src.States
 {
+    public delegate void ShowDamage();
+
     public class BattleState : IState
     {
+
         private readonly GameManager game;
+
+        public event ShowDamage AttackMade;
 
         public BattleState(GameManager game)
         {
@@ -21,30 +27,44 @@ namespace BalkanGame.src.States
         int round = 1;
         ///public Hero currentHero; // TODO: GO to game data ;)
 
-        public void Start()
-        {
+         protected virtual void OnAtackSended() //protected virtual method
+            {
+                ShowDamage?.Invoke(); 
+            }
+
+        public void Start(){
             game.gameInterface.BattleBegin();
             PlayerAtackTurn();
             //display
             BotAtackTurn();
             //display
+            ShowDamage+=OnAtackMaden;
         }
-        public void Destroy()
-        {
+
+        public static void OnAtackMaden()
+    {
+        Console.WriteLine("Atack Maden");
+    } 
+    //where to put the atack !? ;(
+        public void Destroy(){
             throw new NotImplementedException();
         }
 
-        private void Atack(Hero sender)
-        {
+        private void Atack(Hero sender){
             Hero receiver = ChooseHeroForReceiver();
-
+            
+            if(sender.IsHealer)
+            {
+                receiver.HP+sender.damage;
+                game.gameInterface.HealtTeamMate();
+            }
+            else
             //new Atack(sender,receiver); Think about putting atack somewhere? Or if u really need the class atack or if u can make it good with delegate and actions
             receiver.TakeDmg(sender.damage);
             game.gameInterface.AtackReceived();
         }
 
-        private void BotAtackTurn(Team botTeam)
-        {
+        private void BotAtackTurn(Team botTeam){
             foreach (var hero in botTeam.heroes)
             {
                 if (hero is Grandma)
@@ -59,8 +79,7 @@ namespace BalkanGame.src.States
             //TODO: Merge player and bot attacks in one
         }
 
-        private void PlayerAtackTurn(Team playerTeam)
-        {
+        private void PlayerAtackTurn(Team playerTeam){
             foreach (var hero in playerTeam.heroes)
             {
                 if (hero is Grandma)
@@ -73,21 +92,15 @@ namespace BalkanGame.src.States
                 }
             }
         }
-        private Hero RandomHeroFromTeam(Team team)
-        {
+        private Hero RandomHeroFromTeam(Team team)      {
             Random random = new Random();
             int indexOfRandomHero = random.Next(1, team.heroes.Count);
 
             return team.heroes[indexOfRandomHero];
         }
 
-        private void AtackHero(Hero sender)
-        {
-            //Atack(new Atack(sender, ChooseHeroForReceiver()));
-        }
 
-        public Hero ChooseHeroForReceiver()
-        {
+        public Hero ChooseHeroForReceiver()     {
             return null;
             //Invoke menu selected player :) 
         }
